72fd5c547281a73ecf92741376a95444d0228443
Matt Burton <madeofburtonium@gmail.com>
2018-05-10 20:13:29 +0000

    deleted mocking of specific APIs

--- test/mock/index.js
+++
@@ -1,9 +0,0 @@
-const poloniex = require('./poloniex');
-const bitstamp = require('./bitstamp');
-const binance = require('./binance');
-
-module.exports = (fetchMock) => {
-    poloniex(fetchMock);
-    bitstamp(fetchMock);
-    binance(fetchMock);
-};
